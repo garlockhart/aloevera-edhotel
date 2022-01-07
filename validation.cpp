5393d43200da702bb52b0cecb072bb361707e9f5
garlockhart <official.garlynugraha@gmail.com>
2022-01-07 21:53:45 +0700

    Make Validation Program

---
+++ validation.cpp
@@ -0,0 +1,15 @@
+/*
+File Name		: validation.cpp
+Description		: 
+Author			: Garly Nugraha & Nazwa Fitriyani Zahra
+Date			: 08/12/2001
+*/
+
+int ValidationNumbers(int Numbers)
+{
+	if(Numbers < 0){
+		return 0;
+	} else{
+		return 1;
+	}
+}
---
+++ validation.h
@@ -0,0 +1,13 @@
+/*
+File Name		: validation.h
+Description		: 
+Author			: Garly Nugraha & Nazwa Fitriyani Zahra
+Date			: 07/01/2022
+*/
+
+#ifndef validation_H
+#define validation_H
+
+int ValidationNumbers(int Numbers);
+
+#endif
